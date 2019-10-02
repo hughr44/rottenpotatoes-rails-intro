27513a17be68bffa7a4adaa507ffea515b37a496
hughr44 <hugh.r44@gmail.com>
2019-10-02 20:36:54 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,33 +11,33 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #
-    #@all_ratings = ['G','PG','PG-13','R']
     
-    #session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
-    #session[:ratings] = params[:ratings] || session[:ratings]
+    @all_ratings = ['G','PG','PG-13','R']
     
-    #@sort = params[:sort] || session[:sort]
-    #session[:sort] = @sort
+    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
+    session[:ratings] = params[:ratings] || session[:ratings]
     
-    #@movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    @sort = params[:sort] || session[:sort]
+    session[:sort] = @sort
     
-    #if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
-    #  flash.keep
-    #  redirect_to movies_path(sort: session[:sort],ratings: session[:ratings])
-    #elsif !params[:sort].nil? || params[:ratings].nil?
-    #  if !params[:ratings].nil?
-    #    @movies = Movie.where(rating: params[:ratings].keys).order(session[:sort])
-    #  else
-    #    @movies = Movie.all.order(session[:sort])
-    #  end
-    #elsif !session[:ratings].nil? || !session[:sort].nil?
-    #  flash.keep
-    #  redirect_to movies_path(sort: session[:sort],ratings: session[:ratings])
-    #else
-    #  @movies = Movie.all
-    #end
-    @movies = Movie.all
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    
+    if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movies_path(sort: session[:sort],ratings: session[:ratings])
+    elsif !params[:sort].nil? || params[:ratings].nil?
+      if !params[:ratings].nil?
+        @movies = Movie.where(rating: params[:ratings].keys).order(session[:sort])
+      else
+        @movies = Movie.all.order(session[:sort])
+      end
+    elsif !session[:ratings].nil? || !session[:sort].nil?
+      flash.keep
+      redirect_to movies_path(sort: session[:sort],ratings: session[:ratings])
+    else
+      @movies = Movie.all
+    end
+
   end
 
   def new
